c93f4f4d0a6dbdb0e1942d8825337dc3aa2b1bf6
idapt CI <ci@idapt.ai>
2026-04-06 22:50:14 +0000

    chore: sync cli-v2026.04.06.ab939dda from upstream

---
+++ cmd/machine.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var machineRemoteCmd = &cobra.Command{
+	Use:     "machine",
+	Aliases: []string{"m"},
+	Short:   "Manage remote machines",
+}
+
+func init() {
+	machineRemoteCmd.AddCommand(machineListCmd)
+	machineRemoteCmd.AddCommand(machineCreateCmd)
+	machineRemoteCmd.AddCommand(machineGetCmd)
+	machineRemoteCmd.AddCommand(machineEditCmd)
+	machineRemoteCmd.AddCommand(machineStartCmd)
+	machineRemoteCmd.AddCommand(machineStopCmd)
+	machineRemoteCmd.AddCommand(machineTerminateCmd)
+	machineRemoteCmd.AddCommand(machineActivityCmd)
+	machineRemoteCmd.AddCommand(machineExecCmd)
+	machineRemoteCmd.AddCommand(machineTmuxCmd)
+	machineRemoteCmd.AddCommand(machineFileCmd)
+	machineRemoteCmd.AddCommand(machineDirCmd)
+	machineRemoteCmd.AddCommand(machineFwCmd)
+	machineRemoteCmd.AddCommand(machineUserCmd)
+	machineRemoteCmd.AddCommand(machinePortCmd)
+	machineRemoteCmd.AddCommand(machineEnvVarCmd)
+}
